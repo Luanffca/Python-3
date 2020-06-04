f531ab3015ee9f8122758fd0ee53d49d4a0b9afb
Luanffca <francaluan280@gmail.com>
2020-06-04 13:55:44 -0300

    Update d001.py

---
+++ Mundo_1/d001.py
@@ -0,0 +1,6 @@
+# Desafio 001
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagemm de boas-vidas.
+
+nome = str(input("Digite seu nome? "))
+
+print("É um prazer te conhecer, {}!".format(nome))
