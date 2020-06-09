e84fbe0234741b010bbbed564c8a1153acbc16dc
Luanffca <francaluan280@gmail.com>
2020-06-09 15:19:59 -0300

    Mundo - 2

--- Mundo_2/d051.py
+++ Mundo_2/d051.py
@@ -1,1 +1,13 @@
 ''' Faça um programa que leia um número inteiro e diga se ele é ou não é um número primo. '''
+
+num = int(input("Digite um número: "))
+x = 0
+for c in range(1, num):
+    if num % c == 0:
+        x = x + 1
+        if x > 1:
+          break
+if x > 1:
+  print("Não é primo.")
+else:
+  print("É primo.")
--- Mundo_2/d053.py
+++ Mundo_2/d053.py
@@ -1,1 +1,9 @@
-''' Crie um programa que leia o ano de nascimento de sete pessoas. No final mostre quantas pessoas ainda não atingiram a maioridade e quantas já são miores.'''
+''' Crie um programa que leia o ano de nascimento de sete pessoas. No final mostre quantas pessoas ainda não atingiram a maioridade e quantas já são miores.
+Maior idade 21 anos'''
+from datetime import date
+
+for c in range(1,8):
+    ano = int(input("Digite o ano de seu nascimento:"))
+    y = date.today().year - ano
+    if y < 21:
+        print(ano)
