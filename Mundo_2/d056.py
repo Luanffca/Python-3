dff1c67057a97724a03688454263295733fb4343
Luanffca <francaluan280@gmail.com>
2020-06-30 15:20:06 -0300

    Mundo - 2

--- Mundo_2/d056.py
+++ Mundo_2/d056.py
@@ -1,1 +1,8 @@
 ''' Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores se for "M" ou "F". Caso esteja errado peça a digitação novamente até ter um valor correto.'''
+
+sexo = str(input("Digite M ou F, refente ao seu sexo: ")).upper().strip()
+
+while sexo not in "MF":
+    sexo = str(input("Informação inválida. Por favor, informe seu sexo: ")).upper().strip()
+print("Sexo {} registrado com sucesso.".format(sexo))
+    
--- Mundo_2/d057.py
+++ Mundo_2/d057.py
@@ -1,7 +1,6 @@
 ''' Melhore o jogo do DESAFIO 027 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinha até acerta, mostrando no final quantos palpites foram necessários para vencer.'''
 
 from random import randint
-
 num = randint(0, 10)
 n = 1
 soma = 0
