bfadff6184aa810155167ea34ea4f1e9baf9b39b
Luanffca <francaluan280@gmail.com>
2020-07-28 21:16:14 -0300

    Mundo - 2

--- Mundo_2/d68.py
+++ Mundo_2/d68.py
@@ -2,3 +2,26 @@
 A - Quantas pessoas tem mais de 18 anos. 
 B - Quantos homens foram cadastrados. 
 C - Quantas mulheres tem menos de 20 anos. '''
+mulher = masculino = maior = 0
+while True:
+    print("-+"*20)
+    idade = int(input("Qual sua idade: "))
+    sexo = " "
+    while sexo not in 'MF':
+        sexo = str(input("Sexo: ")).upper().strip()[0]
+    if idade >= 18:
+        maior += 1
+    if sexo == "M":
+        masculino += 1
+    if sexo == "F" and idade < 20:
+        mulher += 1
+    resposta = " "
+    while resposta not in "SN":
+        resposta = str(input("Deseja continua S/N: ")).upper().strip()[0]
+    if resposta == "N":
+        break
+print("-+"*20)
+print(f"Tem {maior} pessoas com mais de 18 anos.")
+print(f"Tem {masculino} homens cadastrados.")
+print(f"Tem {mulher} mulheres com menos de 20 anos.")
+print("-+"*20)
--- Mundo_2/d69.py
+++ Mundo_2/d69.py
@@ -1,4 +1,30 @@
-''' Crie um programa que leia o nome e o preço de vários produtos. O programa deverá se o usuário vai continuar. No final, mostre:
+''' Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre:
 A - Qual é o total gasto na compra. 
 B - Quantos produtos custava mais de R$ 1000. 
 C- Qual e o nome do produto mais barato. '''
+total = maior = barato = cont = 0
+nomepro = ''
+while True:
+    print("-+"*20)
+    nome = str(input("Nome do Produto: "))
+    preço = float(input("Valor do Produto: R$ "))
+    cont += 1
+    total += preço
+    if preço > 1000:
+        maior += 1
+    if cont == 1:
+        barato = preço
+        nomepro = nome
+    else:
+        if preço < barato:
+            barato = preço
+            nomepro = nome
+    opção = " "
+    while opção not in 'SN':
+        opção = str(input("Deseja continuar S/N: ")).upper().strip()[0]
+    if opção == "N":
+        break
+print("-+"*20)
+print(f"O valor total gasto é de R$ {total}.")
+print(f"Tem {maior} produtos que custa mais de R$1000.")
+print(f"O produto mais barato foi {nomepro} e custa R$ {barato}")
